4e461d316371e40a65ef1a537a9443f29aa64de1
huirui <huirui@newbeescm.com>
2018-06-21 18:16:20 +0800

    提交

--- main.js
+++ main.js
@@ -2,10 +2,20 @@ const {app, BrowserWindow} = require('electron');
 const fs = require('fs');
 const {dialog} = require('electron');
 const {shell} = require('electron');
-console.log(dialog)
+const {globalShortcut} = require('electron');
 let win;
 
 function createWindow() {
+    //监控刷新
+    globalShortcut.register('f5', function() {
+		console.log('f5 is pressed')
+		win.reload()
+	})
+	globalShortcut.register('CommandOrControl+R', function() {
+		console.log('CommandOrControl+R is pressed')
+		win.reload()
+    })
+    
     win = new BrowserWindow({
         width: 800,
         height: 600,
@@ -14,21 +24,21 @@ function createWindow() {
         }
     });
     // win.loadURL('http://www.pinterest.com');
-    win.loadFile('index.html')
+    win.loadFile('index.html');
 
+    win.openDevTools();
     win.on('closed', () => {
         win = null;
     });
 }
 function readHosts() {
-    let hosts = fs.readFile('/Windows/system32/drivers/etc/hosts', (err, res) => {
+    let hosts = fs.readFile('/Windows/system32/drivers/etc/hosts', 'utf8', (err, res) => {
         if (err) throw err;
         dialog.showOpenDialog();
     });
 }
 app.on('ready', () => {
     createWindow();
-    shell.openExternal('https://github.com')
 });
 
 
